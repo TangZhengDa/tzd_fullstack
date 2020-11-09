cb3aa475a90ecb7069f87738dac9a8262f03a08a
TangZhengDa <695698426@qq.com>
2020-11-09 21:25:40 +0800

    2020-11-09

---
+++ js/es6/解构/index.js
@@ -0,0 +1,26 @@
+// let a = 1
+// let b = 2
+// let c = 3
+
+// let [a, b, c] = [1, 2, 3]
+// console.log(a)
+
+//set
+// let arr = [1, 2, 2, 4, 5, 6, 3, 5, 4]
+// let newArr = new Set(arr)
+// console.log(newArr)
+
+// let [x, y, z] = { a: 'a', b: 'b', c: 'c' }
+// console.log(x)
+
+// let [x, y, z] = new Set(['a', 'b', 'c'])
+// console.log(x)
+
+// let [foo = true] = [false]
+// console.log(foo)
+
+// let [x, y = 'b'] = ['a', null]
+// console.log(y)
+
+// let { y, x, z } = { x: 'aaa', y: 'bbb' }
+// console.log(z)
