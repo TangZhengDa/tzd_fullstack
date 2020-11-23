36c0bdfbfd1c9a5254cdf03ebecc6e3918a8f2ad
TangZhengDa <695698426@qq.com>
2020-11-23 16:57:12 +0800

    2020-11-23

--- wxApp/novel-reader/miniprogram/pages/bookSection/bookSection.js
+++ wxApp/novel-reader/miniprogram/pages/bookSection/bookSection.js
@@ -1,4 +1,6 @@
 // miniprogram/pages/bookSection/bookSection.js
+const db = wx.cloud.database()
+const app = getApp()
 Page({
 
   /**
@@ -38,6 +40,7 @@ Page({
         pre: result.pre,
         preAble: result.pre === '' ? true : false,
         nextAble: result.next === '' ? true : false,
+        page: (result.next.split('/')[2]) - 1
 
       })
     })
@@ -51,6 +54,77 @@ Page({
   nextPage() {
     if (this.data.nextAble) return
     this.getSection(this.data.next)
+  },
+  bindPickerChange(e) {
+    let page = parseInt(e.detail.value)
+    // console.log(e);
+    if (page !== this.page) {
+      this.setData({
+        page: page + 1
+      })
+      this.getSection(this.data.pageArray[page].name)
+
+    }
+
+  },
+  //去看小说
+  navtoUrl(e) {
+    let url = e.currentTarget.dataset.url
+    //已经存在书架的书，记录阅读状态
+    if (url) {
+      db.collection('book').where({
+        userId: app.globalData.openid,
+        bookName: this.data.bookDetailData.name
+      }).get().then(res => {
+        let data = res.data || []
+        if (data.length > 0) {
+          if (data[0].bookUrl != url) {
+            const id = data[0]._id || '';
+            db.collection('book').doc(id).update({
+              data: {
+                bookUrl: url
+              }
+            }).then(res => {
+              console.log(res)
+            })
+          }
+        }
+      })
+    }
+    wx.navigateTo({
+      url: `../bookContent/bookContent?url=${url}&name=${this.data.bookDetailData.name}&imgUrl=${this.data.bookDetailData.imgUrl}`
+    })
+  },
+  joinBook(e) {
+    let url = e.currentTarget.dataset.url
+
+    db.collection('book').where({
+      userId: app.globalData.openid,
+      bookName: this.data.bookDetailData.name
+    }).get().then(res => {
+      const data = res.data[0] || []
+      if (data.length == 0) {//没有加入过书架
+        db.collection('book').add({
+          data: {
+            userId: app.globalData.openid,
+            bookName: this.data.bookDetailData.name,
+            bookUrl: url,
+            imgUrl: this.data.bookDetailData.imgUrl
+          }
+        }).then(res => {
+          console.log(res);
+          wx.showToast({
+            icon: 'success',
+            title: '加入成功',
+          }, 3000)
+        })
+      } else {
+        wx.showToast({
+          icon: 'none',
+          title: '本书已在书架，请到书架阅读',
+        })
+      }
+    })
   },
   /**
    * 生命周期函数--监听页面加载
